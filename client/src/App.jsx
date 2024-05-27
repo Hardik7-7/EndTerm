837f938412de610bdb2573d0702c5be0708e3ba3
Hardik7-7 <101696768+Hardik7-7@users.noreply.github.com>
2024-05-28 05:08:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import BookingPage from './pages/BookingPage'
 import BookingsPage from './pages/BookingsPage'
 import  UpdatePassword  from './pages/UpdatePassword'
 
-axios.defaults.baseURL = 'https://frontend-airbnb-teal.vercel.app'
+axios.defaults.baseURL = 'https://endterm-86tf.onrender.com'
 axios.defaults.withCredentials = true;
 function App() {
   return (
