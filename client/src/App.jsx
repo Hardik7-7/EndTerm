74dc7800c84efe44393cf9940abe35666f07c31e
Hardik7-7 <101696768+Hardik7-7@users.noreply.github.com>
2024-05-28 03:27:01 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ import BookingPage from './pages/BookingPage'
 import BookingsPage from './pages/BookingsPage'
 import  UpdatePassword  from './pages/UpdatePassword'
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'https://frontend-airbnb-coral.vercel.app'
 axios.defaults.withCredentials = true;
 function App() {
   return (
