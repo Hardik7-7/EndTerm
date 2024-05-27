6e0b591e17529af94e52ca5edab0cc7bac7f8d07
Hardik7-7 <101696768+Hardik7-7@users.noreply.github.com>
2024-05-28 02:25:10 +0530

    Update PlacePage.jsx

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -12,7 +12,7 @@ export default function PlacePage() {
     if (!id) {
       return;
     }
-    axios.get(`/places/${id}`).then(response => {
+    axios.get(`https://frontend-airbnb-coral.vercel.app/places/${id}`).then(response => {
       setPlace(response.data);
     });
   }, [id]);
